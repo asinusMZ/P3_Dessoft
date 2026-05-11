8868dbbd91a457238561d9f73edeb9f46f7e8797
Matias <matiasfa@al.insper.edu.br>
2026-05-11 10:29:37 -0300

    pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,46 +1,16 @@
-import random
+class pokemon:
+    def __init__(name, hp, defense, moves):
+        self.name = name
+        self.vida = hp
+        self.defense = defense
+        self.moves = moves
+    
+    def take_damage(self, dano):
+        self.vida -= dano
+    
 
-class Move:
-    def __init__(self, name, move_type, power, accuracy, pp):
-        self.name = name          # "Lança-Chamas"
-        self.move_type = move_type  # "Fogo"
-        self.power = power        # 90  (quão forte é)
-        self.accuracy = accuracy  # 100 (% de chance de acertar)
-        self.pp = pp              # 15  (quantas vezes pode usar)
-        self.current_pp = pp      # PP atual (vai diminuindo)
-
-    def use(self):
-        if self.current_pp > 0:
-            self.current_pp -= 1
+    def is_fainted(self, hp):
+        if hp <= 0:
             return True
-        return False  # sem PP, não pode usar
-
-class battle:
-    def __init__(self, player_pokemon, enemy_pokemon):
-        self.player = player_pokemon
-        self.enemy = enemy_pokemon
-        self.state = "ESCOLHA_ACAO"
-        self.message = ""
-
-    
-    def player_attack(self, move):
-        damage = self.calculate_damage(move, self.player, self.enemy)
-        self.enemy.take_damage(damage)
-        self.message = f"{self.player.name} usou {move.name}!"
-        self.state = "TURNO_INIMIGO"
-
-    def enemy_attack(self):
-        move = random.choice(self.enemy_moves)
-        damage = self.calculate_damage(move, self.enemy, self.player)
-        self.player.take_damage(damage)
-        self.message = f"{self.enemy.name} usou {move.name}"
-    
-    def calculate_damage(self, move, attacker, defender):
-        return int((move.power * attacker.attack) / defender.defense * 0.5)
-    
-    def check_winner(self):
-        if self.enemy.is_fainted():
-            return "JOGADOR"
-        if self.player.is_fainted():
-            return "INIMIGO"
-        return None
+        else:
+            return False
