1342e653cddde85e3fc741d8f3216b2d76ac0fcb
Lucas Queiroz <lucaslq1807@gmail.com>
2026-05-11 09:23:44 -0300

    movimento player 1

--- player.py
+++ player.py
@@ -1,5 +1,19 @@
 import pygame
 
-class Player():
-    
-       
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.height = 64
+        self.width = 64
+        self.speed = 8
+
+    def movimento(self, keys):
+        if keys[pygame.K_w]:
+            self.y += self.speed
+        if keys[pygame.K_s]:
+            self.y -= self.speed
+        if keys[pygame.K_a]:
+            self.x += self.speed
+        if keys[pygame.K_d]:
+            self.x -= self.speed
