33f8c4d6b8bdced7666aa277cfa48819c9f8ba73
lindquist99 <gabrielfl3@al.insper.edu.br>
2026-05-18 10:52:35 -0300

    inicio

--- inicio.py
+++ inicio.py
@@ -5,4 +5,14 @@ def tela_inicio():
     inicio = pygame.image.load("assets/inicio.png").convert()
     return inicio
 
+def draw_inicio(surface, inicio,):
+    surface.blit(inicio, (-158, -144))
+
+
+
+
+
+
+
+
 
