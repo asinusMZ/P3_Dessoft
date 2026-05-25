3bbafc15ef79e3ee71f9c1b0f086283734f41934
lindquist99 <gabrielfl3@al.insper.edu.br>
2026-05-25 11:04:56 -0300

    anotaçoes

--- npc.py
+++ npc.py
@@ -2,6 +2,7 @@ import pygame
 from spritesheet import SpriteSheet
  
 class NPC:
+    # Função executada quando o NPC é criado
     def __init__(self, x, y, image_path=None):
         self.x = x
         self.y = y
@@ -9,12 +10,13 @@ class NPC:
         self.height = 16
  
         self.load_sprite()
- 
+    
+    # Função executada para colocar o sprite do npc
     def load_sprite(self):
         sheet = SpriteSheet("assets/pokecharacters.png", (255, 127, 39))
  
         self.image = sheet.get_sprite(26, 306, 15, 15)
- 
+
     def draw(self, surface, camera_x, camera_y):
         surface.blit(self.image, (self.x - camera_x, self.y - camera_y))
  
