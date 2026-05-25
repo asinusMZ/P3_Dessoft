1fb09b321965aeb6af25fdaa4080a732b89ddd3e
Matias <matiasfa@al.insper.edu.br>
2026-05-25 10:56:12 -0300

    anotacoes 2

--- inicio.py
+++ inicio.py
@@ -1,11 +1,11 @@
 
 import pygame
 
-def tela_inicio():
+def tela_inicio(): # cria tela de inicio
     inicio = pygame.image.load("assets/inicio.png").convert()
     return inicio
 
-def draw_inicio(surface, inicio,):
+def draw_inicio(surface, inicio,): # desenha tela de inicio
     surface.blit(inicio, (0, 0))
 
 
